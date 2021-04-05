1d468fd1a3ba1b6b05ed0b78f537efe168b672ef
hansilyogi <hansilyogi@gmail.com>
2021-04-05 11:02:51 +0530

    Notify all api for news 05-04-2021

---
+++ model/offerModel.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+
+const offerSchema = mongoose.Schema({
+    title: {
+        type: String,
+    },
+    bannerImage: {
+        type: String,
+    },
+    userId : {
+        type: mongoose.Types.ObjectId, ref: "UsersList",
+    },
+    city :{
+        type: mongoose.Types.ObjectId, ref: "city",
+    },
+    dateTime: {
+        type: String,
+    },
+    type:{
+        type: String,
+    },
+    details: {
+        type: String,
+    },
+    redeemBy: {
+        type: String
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+    businessCategory: {
+        type: mongoose.Types.ObjectId, ref: "BusinessCategory",
+    },
+    offerExpire: {
+        type: String
+    },
+    daysRemain: {
+        type: String
+    },
+    faceBook : {
+        type: String,
+        default : ""
+    },
+    instagram : {
+        type: String,
+        default : ""
+    },
+    linkedIn : {
+        type: String,
+        default: ""
+    },
+    twitter : {
+        type: String,
+        default: ""
+    },
+    whatsApp : {
+        type: String,
+        default: ""
+    },
+    youTube : {
+        type: String,
+        default: ""
+    },
+    mail : {
+        type : String,
+        default : "",
+    }
+});
+
+module.exports = mongoose.model("offer", offerSchema);
